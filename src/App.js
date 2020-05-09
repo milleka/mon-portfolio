c167175ef29db896877384ce54574e989967257f
Chassaing Camille <camillechassaing.jdb@gmail.com>
2020-05-09 10:22:20 +0200

    mise en place de la structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='App-header'>
+      <img src="img/logo.png" alt="mon logo"/>
+    </div>
+    </>
+  )
+}
+
+export default App;
